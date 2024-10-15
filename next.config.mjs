a84ba21f2fcaf9295d6bb111f56f256ebf2257be
Samuel <samuferfort@gmail.com>
2024-10-15 21:03:01 +0200

    add cloudinary domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["res.cloudinary.com"], // Add Cloudinary domain here
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        search: "",
+      },
+    ],
   },
 };
 
